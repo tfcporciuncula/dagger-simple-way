1b7657f952b349b4b8093990535f20902337c4c4
Fred Porciúncula <fred@blinkist.com>
2018-12-16 18:43:19 +0100

    Fix instrumentation tests after androidx migration

--- app/src/androidTest/java/com/fredporciuncula/daggersimpleway/posts/BestPostActivityTest.kt
+++ app/src/androidTest/java/com/fredporciuncula/daggersimpleway/posts/BestPostActivityTest.kt
@@ -1,11 +1,11 @@
 package com.fredporciuncula.daggersimpleway.posts
 
-import androidx.test.InstrumentationRegistry
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import com.fredporciuncula.daggersimpleway.R
 import com.fredporciuncula.daggersimpleway.test.LazyActivityTestRule
 import io.appflate.restmock.RESTMockServer
@@ -29,7 +29,10 @@ class BestPostActivityTest {
     @get:Rule var activityRule = LazyActivityTestRule(BestPostActivity::class.java)
 
     @Before fun setup() {
-        RESTMockServerStarter.startSync(AndroidAssetsFileParser(InstrumentationRegistry.getContext()), AndroidLogger())
+        RESTMockServerStarter.startSync(
+            AndroidAssetsFileParser(InstrumentationRegistry.getInstrumentation().context),
+            AndroidLogger()
+        )
     }
 
     @After fun tearDown() = RESTMockServer.shutdown()
